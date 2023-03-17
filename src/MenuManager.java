924a74559aecb4111004671fb93ae1959eb5fb11
gujaemin <woalsrn@naver.com>
2023-03-17 18:30:24 +0900

    added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,17 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("1. Add books");//책 추가
+		System.out.println("2. Delete books");//책 삭제
+		System.out.println("3. Edit books");//책 편집
+		System.out.println("4. View books");//
+		System.out.println("5. Exit");//종료
+		
+		
+
+	}
+
+}
